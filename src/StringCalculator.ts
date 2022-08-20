09cc4fad1f3e45300183497582c55b4360a2968d
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 01:16:23 +0530

    refactor: make add() method of StringCalculator to work with alphabets and numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,10 +1,16 @@
 class StringCalculator {
     public add(numbers: string): number {
         const enteredNumbers: string = numbers.trim();
-        let sum: number = 0;
+        let sum: number;
         if (enteredNumbers.includes(',')) {
             sum = enteredNumbers.split(',')
-                .reduce((sum: number, currStrNo: string) => (sum + (+currStrNo)), 0);
+                .reduce((sum: number, currStrNo: string) => {
+                    if (isNaN(+currStrNo)) {
+                        return (sum + (currStrNo.charCodeAt(0) - 96))
+                    } else {
+                        return (sum + (+currStrNo))
+                    }
+                }, 0);
         } else {
             sum = +enteredNumbers;
         }

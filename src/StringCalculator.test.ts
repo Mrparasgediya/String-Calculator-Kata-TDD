12a32da4f8e059b72951fadb692ad02f7958459c
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 00:29:40 +0530

    refector: add test suit and add beforeEach() in StringCalculator tests

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -1,26 +1,33 @@
 import StringCalculator from './StringCalculator'
 
-it("should return zero if string is empty", () => {
-    const stringCalculator: StringCalculator = new StringCalculator();
-    const inputValue: string = "";
-    const expectedValue: number = +inputValue;
-    const resultValue: number = stringCalculator.add(inputValue);
-    expect(resultValue).toBe(expectedValue)
-})
+describe("StringCalculator", () => {
+    describe('add()', () => {
+        let stringCalculator: StringCalculator;
 
-it('should return number itself if only one number is entered as argument', () => {
-    const stringCalculator: StringCalculator = new StringCalculator();
-    const inputValue: string = "2";
-    const expectedValue: number = +inputValue;
-    const resultvalue: number = stringCalculator.add(inputValue);
-    expect(resultvalue).toBe(expectedValue);
-})
+        beforeAll(() => {
+            stringCalculator = new StringCalculator();
+        })
 
-it('should return addition of entered two numbers seperated by ","', () => {
-    const stringCalculator: StringCalculator = new StringCalculator();
-    const inputValue: string = "1,2";
-    const expectedValue: number = inputValue.split(',')
-        .reduce((sum: number, currStrNo: string) => (sum + (+currStrNo)), 0);
-    const resultValue: number = stringCalculator.add(inputValue);
-    expect(resultValue).toBe(expectedValue);
+        it("should return zero if string is empty", () => {
+            const inputValue: string = "";
+            const expectedValue: number = +inputValue;
+            const resultValue: number = stringCalculator.add(inputValue);
+            expect(resultValue).toBe(expectedValue)
+        })
+
+        it('should return number itself if only one number is entered as argument', () => {
+            const inputValue: string = "2";
+            const expectedValue: number = +inputValue;
+            const resultvalue: number = stringCalculator.add(inputValue);
+            expect(resultvalue).toBe(expectedValue);
+        })
+
+        it('should return addition of entered two numbers seperated by ","', () => {
+            const inputValue: string = "1,2";
+            const expectedValue: number = inputValue.split(',')
+                .reduce((sum: number, currStrNo: string) => (sum + (+currStrNo)), 0);
+            const resultValue: number = stringCalculator.add(inputValue);
+            expect(resultValue).toBe(expectedValue);
+        })
+    })
 })

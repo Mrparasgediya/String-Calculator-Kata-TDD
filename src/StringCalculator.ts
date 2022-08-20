8a513063405e0aec4698d9b6e130c8f71afcaeb6
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-20 23:55:56 +0530

    feat: add an add() method to StringCalculator Class
    Added add() method in class StringCalculator that returns addition of entered numbers(in string datatype) seperated by delimiter comma

---
+++ src/StringCalculator.ts
@@ -0,0 +1,9 @@
+class StringCalculator {
+    public add(numbers: string): number {
+        const enteredNumbers: string = numbers.trim();
+        if (enteredNumbers === "") {
+            return 0;
+        }
+    }
+}
+export default StringCalculator;

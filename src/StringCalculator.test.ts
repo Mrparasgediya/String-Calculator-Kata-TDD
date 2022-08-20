97ce3e78ab991244e5fde566137f27e448d627ec
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 00:05:51 +0530

    test: check add() return number itself if there is only one number

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -7,3 +7,11 @@ it("should return zero if string is empty", () => {
     const resultValue: number = stringCalculator.add(inputValue);
     expect(resultValue).toBe(expectedValue)
 })
+
+it('should return number itself if only one number is entered as argument', () => {
+    const stringCalculator: StringCalculator = new StringCalculator();
+    const inputValue: string = "2";
+    const expectedValue: number = +inputValue;
+    const resultvalue: number = stringCalculator.add(inputValue);
+    expect(resultvalue).toBe(expectedValue);
+})

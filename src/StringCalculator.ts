28fe1004ea7a66a60e0a2fbcab2323c19372c312
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 00:20:10 +0530

    refactor: make add() to work with two numbers
    make add() method of class StringCalculator to return addition of entered two number seperated by ,(comma)

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,10 +1,14 @@
 class StringCalculator {
     public add(numbers: string): number {
         const enteredNumbers: string = numbers.trim();
-        if (enteredNumbers === "") {
-            return 0;
+        let sum: number = 0;
+        if (enteredNumbers.includes(',')) {
+            const splittedArray: string[] = enteredNumbers.split(',');
+            return ((+splittedArray[0]) + (+splittedArray[1]));
+        } else {
+            sum = +enteredNumbers;
         }
-        return +enteredNumbers;
+        return sum;
     }
 }
 export default StringCalculator;

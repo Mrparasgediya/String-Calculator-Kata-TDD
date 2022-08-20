a617850ad586c860f609486017389d1c382d1261
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 01:38:16 +0530

    refactor: make add() method of stringCalculator to throw error if negative number is entered
    make add() method to throw 'Negatives not allowed : negativeNumber' if entered numbers contains negative number

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -3,14 +3,18 @@ class StringCalculator {
         const enteredNumbers: string = numbers.trim();
         let sum: number;
         if (enteredNumbers.includes(',')) {
-            sum = enteredNumbers.split(',')
-                .reduce((sum: number, currStrNo: string) => {
-                    if (isNaN(+currStrNo)) {
-                        return (sum + (currStrNo.charCodeAt(0) - 96))
-                    } else {
-                        return (sum + (+currStrNo))
-                    }
-                }, 0);
+            sum = 0;
+            for (let currStrNo of enteredNumbers.split(",")) {
+                // check number is negative
+                if (+currStrNo < 0)
+                    throw new Error("Negatives not allowed : " + currStrNo)
+                // check number is alphabet
+                if (isNaN(+currStrNo)) {
+                    sum += (currStrNo.charCodeAt(0) - 96);
+                } else {
+                    sum += (+currStrNo);
+                }
+            }
         } else {
             sum = +enteredNumbers;
         }

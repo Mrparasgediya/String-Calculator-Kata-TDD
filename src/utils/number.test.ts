db929557662a18a01ad9efc5d44333db01dffc44
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-22 04:22:13 +0530

    test: check getSumOfElementsByAdditionMethod() return sum of all numbers if AdditionMethod is DEFAULT
    add test that check getSumOfElementsByAdditionMethod() of number utils returns sum of all numbers if addition method is DEFAULT

--- src/utils/number.test.ts
+++ src/utils/number.test.ts
@@ -19,16 +19,21 @@ describe('number utils', () => {
         })
     })
     describe('getSumOfElementsByAdditionMethod', () => {
+        const inputValue: number[] = [1, 2, 3, 4];
+
         it('should return sum of even index numbers if addition method is EVEN', () => {
-            const inputValue: number[] = [1, 2, 3, 4];
             const expectedValue: number = 4;
             const resultValue: number = getSumOfElementsByAdditionMethod(inputValue, AdditionMethods.EVEN);
             expect(resultValue).toBe(expectedValue);
         })
         it('should return sum of odd index numbers if addition method is ODD', () => {
-            const inputValue: number[] = [1, 2, 3, 4];
             const expectedValue: number = 6;
             const resultValue: number = getSumOfElementsByAdditionMethod(inputValue, AdditionMethods.ODD);
+            expect(resultValue).toBe(expectedValue)
+        })
+        it('should return sum of all index numbers if addition method is DEFAULT', () => {
+            const expectedValue: number = 10;
+            const resultValue: number = getSumOfElementsByAdditionMethod(inputValue, AdditionMethods.DEFAULT);
             expect(resultValue).toBe(expectedValue)
         })
     })

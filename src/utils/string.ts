7fb2fbb65615768cfabaec75eeb645bce205c054
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 22:39:52 +0530

    refactor: make getDelimitersArr() to work with delimiter format "//[delimiter]\n[numbers...]"
    now getDelimitersArr() of string utils work with given custom delimter format "//[delimiter]\n[numbers..]" ex:"//[==]\n1==2==3==4"

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,8 +4,14 @@ export const getDelimitersArr = (string: string = '', defaultDelimiter: string =
     let enteredString: string = string;
     if (enteredString.startsWith('//')) {
         // add custom delimiter to array
-        const customDelimiter: string = enteredString.substring(2, enteredString.indexOf('\n'));
-        delimitersArr.push(customDelimiter);
+        const customDelimiterBox: string = enteredString.substring(2, enteredString.indexOf('\n'));
+        // check delimiter is in format [delimiter]
+        if (customDelimiterBox.startsWith("[") && customDelimiterBox.endsWith("]")) {
+            const delimiter: string = customDelimiterBox.substring(1, customDelimiterBox.indexOf(']'));
+            delimitersArr.push(delimiter);
+        } else {
+            delimitersArr.push(customDelimiterBox);
+        }
         // remove custom delimiter strings from enteredString
         enteredString = enteredString.substring(enteredString.indexOf("\n") + 1);
     } else {

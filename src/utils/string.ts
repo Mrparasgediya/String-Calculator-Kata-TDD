f00dfec3cbfb882f477c1cc901de1f33bdf2d9e0
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 22:32:40 +0530

    refactor: make getDelimitersArr() to work with custom delimiter format: "//delimiter\n[numbers..]"
    now getDelimitersArr() of string utils returns array with custom delimiter if string is entered in "//delimiter\n[numbers...]" format

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,17 @@
 
 export const getDelimitersArr = (string: string = '', defaultDelimiter: string = ","): string[] => {
-    const delimitersArr: string[] = [defaultDelimiter];
-    if (string.includes("\n")) {
+    const delimitersArr: string[] = [];
+    let enteredString: string = string;
+    if (enteredString.startsWith('//')) {
+        // add custom delimiter to array
+        const customDelimiter: string = enteredString.substring(2, enteredString.indexOf('\n'));
+        delimitersArr.push(customDelimiter);
+        // remove custom delimiter strings from enteredString
+        enteredString = enteredString.substring(enteredString.indexOf("\n") + 1);
+    } else {
+        delimitersArr.push(defaultDelimiter);
+    }
+    if (enteredString.includes("\n")) {
         delimitersArr.push('\n');
     }
     return delimitersArr;

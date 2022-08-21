74fe7c45e018685d7577f86580352f81d28543c3
09_Paras_Gediya <mrparas103@gmail.com>
2022-08-21 21:17:25 +0530

    test: check getDelimitersArr() should return \n delimiter if string contains \n
    test getDelimitersArr() of string utils that returns array containing default delimiter and "\n" if entered string contains "\n"

--- src/utils/string.test.ts
+++ src/utils/string.test.ts
@@ -7,5 +7,12 @@ describe('string utils', () => {
             const resultValue: string[] = getDelimitersArr();
             expect(resultValue).toEqual(expectedValue);
         })
+
+        it('should return array with delimiter \\n and default delimiter if entered string contains \\n', () => {
+            const inputValue: string = "\n1,2";
+            const expectedValue: string[] = [",", "\n"];
+            const resultValue: string[] = getDelimitersArr(inputValue);
+            expect(resultValue).toEqual(expectedValue);
+        })
     });
 })
